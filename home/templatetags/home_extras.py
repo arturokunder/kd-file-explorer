ac2daf524e549a28fba1e86e1164c5d1a56beb98
Arturo <arturo@kunder.cl>
2014-07-07 11:32:28 -0400

    fix error que mostraba carpetas como archivos

--- home/templatetags/home_extras.py
+++ home/templatetags/home_extras.py
@@ -14,3 +14,20 @@ def formatfilename(value):
       return value
     else:
       return re.sub(r'[ ]{0,1}[0-9a-fA-F]{24}', '', value.replace('-', ' '))
+
+@register.filter
+def checkValidObject(obj):
+  print obj.is_subdir
+  print obj.is_file
+  print obj.basename
+  print re.match(r'^.*\.(\w+)$', obj.basename)
+
+  if obj.is_subdir:
+    print 'return true'
+    return True
+  elif obj.is_file and re.match(r'^.*\.(\w+)$', obj.basename):
+    print 'return true'
+    return True
+  else:
+    print 'return false'
+    return False
