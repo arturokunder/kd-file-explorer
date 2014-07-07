81681ac178cad066c6e16b564ee8a4e2065302f4
Pedro Cortez <pcortez@gmail.com>
2014-07-07 12:46:49 -0400

    static files

--- fileExplorer/urls.py
+++ fileExplorer/urls.py
@@ -1,5 +1,5 @@
 from django.conf.urls import patterns, include, url
-
+from fileExplorer import settings
 from django.contrib import admin
 admin.autodiscover()
 
@@ -11,5 +11,8 @@ urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
     url(r'^login/$', 'django.contrib.auth.views.login', {'template_name': 'login.jade'}),
     url(r'^', include('cloud_browser.urls')),
+)
 
+urlpatterns += patterns('',
+    (r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.STATIC_ROOT}),
 )
