8af1fc3670941d3d2f2ac20b406e96643ba553b2
arturokunder <arturokunder@users.noreply.github.com>
2015-08-07 17:54:51 -0300

    fix urls

--- fileExplorer/urls.py
+++ fileExplorer/urls.py
@@ -10,7 +10,7 @@ urlpatterns = patterns('',
 
     url(r'^admin/', include(admin.site.urls)),
     url(r'^login/$', 'django.contrib.auth.views.login', {'template_name': 'login.jade'}),
-    url(r'^logout/$', 'django.contrib.auth.views.logout', {'next_page': '/login/'})
+    url(r'^logout/$', 'django.contrib.auth.views.logout', {'next_page': '/login/'}),
     url(r'^', include('cloud_browser.urls')),
 )
 
